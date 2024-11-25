5304fa990d1520b4926b887cbe7caa9bdf258295
Patrick Irving <psirving@gmail.com>
2024-11-25 05:07:28 +0000

    test: add tests to cover all modules

--- tests/test_pycounts.py
+++ tests/test_pycounts.py
@@ -1,31 +1,54 @@
-from pycounts.pycounts import count_words
+from psirving_pycounts.pycounts import count_words
+from psirving_pycounts.plotting import plot_words
+from psirving_pycounts.datasets import get_flatland
+import matplotlib
 from collections import Counter
+import pytest
 
-def test_count_words():
-    """Plot a bar chart of word counts.
-    
-    Parameters
-    ----------
-    word_counts : collections.Counter
-        Counter object of word counts.
-    n : int, optional
-        Plot the top n words. By default, 10.
 
-    Returns
-    -------
-    matplotlib.container.BarContainer
-        Bar chart of word counts.
 
-    Examples
-    --------
-    >>> from pycounts.pycounts import count_words
-    >>> from pycounts.plotting import plot_words
-    >>> counts = count_words("text.txt")
-    >>> plot_words(counts)
-    """
-    expected = Counter({'insanity': 1, 'is': 1, 'doing': 1, 
-                        'the': 1, 'same': 1, 'thing': 1, 
-                        'over': 2, 'and': 2, 'expecting': 1,
-                        'different': 1, 'results': 1})
+@pytest.fixture
+def einstein_counts():
+    return Counter({'insanity': 1, 'is': 1, 'doing': 1, 
+                    'the': 1, 'same': 1, 'thing': 1, 
+                    'over': 2, 'and': 2, 'expecting': 1,
+                    'different': 1, 'results': 1})
+
+def test_count_words(einstein_counts):
+    """Test count_words function."""
     actual = count_words("tests/einstein.txt")
-    assert actual == expected, "Einstein quote counted incorrectly!"
+    assert actual == einstein_counts, "Einstein quote counted incorrectly!"
+
+
+def test_plot_words(einstein_counts):
+    """Test plotting of word counts."""
+    fig = plot_words(einstein_counts)
+    assert isinstance(fig, matplotlib.container.BarContainer), \
+           "Wrong plot type"
+    assert len(fig.datavalues) == 10, \
+           "Incorrect number of bars plotted"
+
+
+@pytest.mark.parametrize("obj", [3.141, "test.txt", ["list", "of", "words"]])
+def test_plot_words_error(obj):
+    """Check TypeError raised when Counter not used."""
+    with pytest.raises(TypeError):
+        list_object = ["Pythons", "are", "non", "venomous"]
+        plot_words(list_object)
+
+
+def test_integration():
+    """Test count_words() and plot_words() workflow."""
+    counts = count_words("tests/einstein.txt")
+    fig = plot_words(counts)
+    assert isinstance(fig, matplotlib.container.BarContainer), "Wrong plot type"
+    assert len(fig.datavalues) == 10, "Incorrect number of bars plotted"
+    assert max(fig.datavalues) == 2, "Highest word count should be 2"
+
+
+
+def test_regression():
+    """Regression test for Flatland"""
+    top_word = count_words(get_flatland()).most_common(1)
+    assert top_word[0][0] == "the", "Most common word is not 'the'"
+    assert top_word[0][1] == 2262, "'the' count has changed"
